9baa9fbb1e56ab7481014d9c2e2491efc308e75c
evelynzz <zhao.yue3@northeastern.edu>
2023-04-09 11:29:23 -0700

    a9 Mongo sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //const TUITS_API = 'http://localhost:4000/api/tuits';
 //const TUITS_API = 'https://tuiter-node-server-app-8tlp.onrender.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = 'http://localhost:4000/api';
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
